d72ef0ad646640b6354085bd7fe0be7c5d99e491
gerry <hkggerry@gmail.com>
2021-11-14 17:59:47 -0800

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.children[1].value)
+  })
+})
+
+function handleToDo(toDoList){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = toDoList
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
